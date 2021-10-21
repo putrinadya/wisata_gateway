d551af0a41a5c42a0ecb3caaea911ff00d515a88
Nadya <nadya@codelabs.co.id>
2021-10-21 10:14:42 +0700

    update 21/10/2021

--- app/Http/Controllers/Cms/EmployeeController.php
+++ app/Http/Controllers/Cms/EmployeeController.php
@@ -36,51 +36,80 @@ class EmployeeController extends Controller
     public function store(Request $request)
     {
         // dd('test');
-        $client = new \GuzzleHttp\Client();;
-        $request = $client->post('http://localhost:8002/Employee/store');
-       
-        $response = $request->getBody()->getContents();
         
-        return $data= json_decode($response, true);
+        // $request = $client->post('http://localhost:8002/Employee/store');
+       
+        // $response = $request->getBody()->getContents();
+        
+        // return $data= json_decode($response, true);
 
-        print("<pre>".print_r($data, true). "</pre>");
+        // print("<pre>".print_r($data, true). "</pre>");
 
-        // $response = $client->request('POST', 'http://localhost:8002/Employee/store', [
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', 'http://localhost:8002/Employee/store', [
+            'json' => [
+                'user_roles_id' => '3',
+                'employee_firstname' => 'Putri',
+                'employee_middlename' => 'Nadya',
+                'employee_lastname' => 'En',
+                'employee_username' => 'Putrinadyaen',
+                'employee_password' => '1234567',
+                'employee_email' => 'putrinadya@gmail.com',
+                'employee_status' => '1',
+                'employee_image' => '',
+                'created_by' => '1',
+                'update_by' => '1'
+            ]
+        ]);
+        $body = $response->getBody();
+        return $body_array = json_decode($body, true);
+        print("<pre>".print_r($body, true). "</pre>");
+    
+    }
+
+    public function update(Request $request, $id)
+    {
+        
+        // $request = $client->put('http://localhost:8002/Employee/update');
+        // $response = $request->getBody()->getContents();
+        
+        // return $data = json_decode($response, true);
+
+        // print("<pre>".print_r($data, true). "</pre>");
+
+        
+        // $client = new \GuzzleHttp\Client();
+        // $response = $client->request('PUT', 'http://localhost:8002/Employee/store', [
         //     'json' => [
-        //         'user_roles_id' => '2',
-        //         'employee_firstname' => 'Putri',
-        //         'employee_middlename' => 'Nadya',
-        //         'employee_lastname' => 'En',
-        //         'employee_username' => 'Putrinadyaen',
-        //         'employee_password' => '1234567',
-        //         'employee_email' => 'putrinadya@gmail.com',
+        //         'user_roles_id' => '3',
+        //         'employee_firstname' => 'Putri3',
+        //         'employee_middlename' => 'Nadya3',
+        //         'employee_lastname' => 'En3',
+        //         'employee_username' => 'Putrinadyaen3',
+        //         'employee_password' => '12345678',
+        //         'employee_email' => 'putrinadya@gmail.com3',
         //         'employee_status' => '1',
         //         'employee_image' => '',
         //         'created_by' => '1',
         //         'update_by' => '1'
         //     ]
         // ]);
-    //     $body = $response->getBody();
-    //     return $body_array = json_decode($body);
-    //     print_r($body_array);
-    // 
-    }
+        // $body = $response->getBody();
+        // return $body_array = json_decode($body, true);
+        // print("<pre>".print_r($body, true). "</pre>");
 
-    public function update(Request $request, $id)
-    {
-        $client = new \GuzzleHttp\Client();;
-        $request = $client->put('http://localhost:8002/Employee/update');
-        $response = $request->getBody()->getContents();
-        
-        return $data = json_decode($response, true);
-
-        print("<pre>".print_r($data, true). "</pre>");
+        $endpoint = url('PUT') . 'http://localhost:8002/Employee/store' . $request->id;
+        $body = json_encode($request->all());
+        $client = new Client();
+        $response = $client->put($endpoint, ['body' => json_encode($request->all())]);    
+        return redirect()->action('EmployeeController@update');
+    
     }
 
     public function delete($id)
     {
        $client = new \GuzzleHttp\Client();;
-        $request = $client->delete('http://localhost:8002/Employee/delete');
+        $request = $client->delete('http://localhost:8002/Employee/delete'.$id);
         $response = $request->getBody()->getContents();
         
         return $data = json_decode($response, true);
